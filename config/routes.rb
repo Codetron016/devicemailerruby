1dc8a54261debcf81b814a9374dc83814c9c3592
Davis Sam <shotime013@gmail.com>
2017-09-14 05:08:05 +0000

    navbar, user registration, and user edit update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
  
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#index'
-
   # get 'hello', to: 'pages#contact' <-- how to redirect to pages on website with specific words.
-  
   get 'pages/contact'
-
   get 'pages/about'
 
   
