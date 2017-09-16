56290a6eafe86502f1a9300967c45bf645fb8d0c
Davis Sam <shotime013@gmail.com>
2017-09-16 21:15:27 +0000

    scaffold crud articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
   devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :articles
   root to: 'pages#index'
   # get 'hello', to: 'pages#contact' <-- how to redirect to pages on website with specific words.
   get 'pages/contact'
